ba4f4f701c2d69a434c6e6f70e8abd1b9f70eb8f
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2021-07-20 05:32:51 +0530

    Added react-bootstrap and friend cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container,Row,Card } from 'react-bootstrap';
+
+const friends=[
+  {name:"Sachin",   pic:"./Sachin.jpg",   quote:"\"Modern\"",   age:21},
+  {name:"Malindu",  pic:"./Malindu.jpg",  quote:"\"Problems\"",        age:21},
+  {name:"Nipun",  pic:"./Nipun.jpg",  quote:"\"Require\"",        age:21},
+  {name:"Sandul",  pic:"./Sandul.jpg",  quote:"\"Modern\"",        age:21},
+  {name:"Izzath",  pic:"./Izzath.jpg",  quote:"\"Solutions\"",        age:21}
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid="md">
+        <Row>
+          {friends.map((friend)=>{
+            return(
+            <Card fluid="md" className="Card" bg="dark" text="white" style={{width:'18rem'}}>
+              <Card.Title>{friend.name}</Card.Title>
+              <Card.Img src={friend.pic} alt={friend.pic} style={{width:'100%'}}/>
+              <Card.Text>{friend.quote}<br/>Age:{friend.age}</Card.Text>
+            </Card>
+          )})}
+        </Row>
+      </Container>
     </div>
   );
 }
