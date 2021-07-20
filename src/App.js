8586175acd2c42b09b0b8475998c4aa1e8771add
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2021-07-20 05:49:24 +0530

    some cleanup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { Container,Row,Card } from 'react-bootstrap';
 
 const friends=[
-  {name:"Sachin",   pic:"./Sachin.jpg",   quote:"\"Modern\"",   age:21},
-  {name:"Malindu",  pic:"./Malindu.jpg",  quote:"\"Problems\"",        age:21},
-  {name:"Nipun",  pic:"./Nipun.jpg",  quote:"\"Require\"",        age:21},
-  {name:"Sandul",  pic:"./Sandul.jpg",  quote:"\"Modern\"",        age:21},
-  {name:"Izzath",  pic:"./Izzath.jpg",  quote:"\"Solutions\"",        age:21}
+  //To add a new friend use this template;
+  //{name:"", pic:"", quote:"", age:0}
+  {name:"Sachin", pic:"./pics/Sachin.jpg", quote:"\"Modern\"", age:21},
+  {name:"Malindu", pic:"./pics/Malindu.jpg", quote:"\"Problems\"", age:21},
+  {name:"Nipun", pic:"./pics/Nipun.jpg", quote:"\"Require\"", age:21},
+  {name:"Sandul", pic:"./pics/Sandul.jpg", quote:"\"Modern\"", age:21},
+  {name:"Izzath", pic:"./pics/Izzath.jpg", quote:"\"Solutions\"", age:21}
 ];
 
 function App() {
@@ -18,7 +20,7 @@ function App() {
             return(
             <Card fluid="md" className="Card" bg="dark" text="white" style={{width:'18rem'}}>
               <Card.Title>{friend.name}</Card.Title>
-              <Card.Img src={friend.pic} alt={friend.pic} style={{width:'100%'}}/>
+              <Card.Img src={friend.pic} alt={friend.pic}/>
               <Card.Text>{friend.quote}<br/>Age:{friend.age}</Card.Text>
             </Card>
           )})}
